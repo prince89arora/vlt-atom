557d2b189d6de5b964d1ef658921473cf5a5ded6
Prince Arora <prnc.arora0@gmail.com>
2017-11-29 11:56:31 +0530

    toggle updated

--- spec/vlt-atom-spec.js
+++ spec/vlt-atom-spec.js
@@ -15,59 +15,5 @@ describe('VltAtom', () => {
     activationPromise = atom.packages.activatePackage('vlt-atom');
   });
 
-  describe('when the vlt-atom:toggle event is triggered', () => {
-    it('hides and shows the modal panel', () => {
-      // Before the activation event the view is not on the DOM, and no panel
-      // has been created
-      expect(workspaceElement.querySelector('.vlt-atom')).not.toExist();
 
-      // This is an activation event, triggering it will cause the package to be
-      // activated.
-      atom.commands.dispatch(workspaceElement, 'vlt-atom:toggle');
-
-      waitsForPromise(() => {
-        return activationPromise;
-      });
-
-      runs(() => {
-        expect(workspaceElement.querySelector('.vlt-atom')).toExist();
-
-        let vltAtomElement = workspaceElement.querySelector('.vlt-atom');
-        expect(vltAtomElement).toExist();
-
-        let vltAtomPanel = atom.workspace.panelForItem(vltAtomElement);
-        expect(vltAtomPanel.isVisible()).toBe(true);
-        atom.commands.dispatch(workspaceElement, 'vlt-atom:toggle');
-        expect(vltAtomPanel.isVisible()).toBe(false);
-      });
-    });
-
-    it('hides and shows the view', () => {
-      // This test shows you an integration test testing at the view level.
-
-      // Attaching the workspaceElement to the DOM is required to allow the
-      // `toBeVisible()` matchers to work. Anything testing visibility or focus
-      // requires that the workspaceElement is on the DOM. Tests that attach the
-      // workspaceElement to the DOM are generally slower than those off DOM.
-      jasmine.attachToDOM(workspaceElement);
-
-      expect(workspaceElement.querySelector('.vlt-atom')).not.toExist();
-
-      // This is an activation event, triggering it causes the package to be
-      // activated.
-      atom.commands.dispatch(workspaceElement, 'vlt-atom:toggle');
-
-      waitsForPromise(() => {
-        return activationPromise;
-      });
-
-      runs(() => {
-        // Now we can test for view visibility
-        let vltAtomElement = workspaceElement.querySelector('.vlt-atom');
-        expect(vltAtomElement).toBeVisible();
-        atom.commands.dispatch(workspaceElement, 'vlt-atom:toggle');
-        expect(vltAtomElement).not.toBeVisible();
-      });
-    });
-  });
 });
--- spec/vlt-atom-view-spec.js
+++
@@ -1,9 +0,0 @@
-'use babel';
-
-import VltAtomView from '../lib/vlt-atom-view';
-
-describe('VltAtomView', () => {
-  it('has one valid test', () => {
-    expect('life').toBe('easy');
-  });
-});
